877c7cb9ac627028a058782fa325303f4f5b033e
13631521719 <598601307@qq.com>
2021-04-06 15:20:45 +0800

    工程初始化

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public @ResponseBody Map test()throws Exception{
+        Map<String,String> map=new HashMap<>();
+        map.put("code","1001");
+        map.put("message","这是返回的消息");
+        return map;
+    }
+}
